0f4de2e48f45a5b9d593d39ff359a34165eb7bfe
Knight <janae.jpeg@gmail.com>
2020-04-01 06:21:56 -0500

    gen update

--- java/pseudojavascript.js
+++ java/pseudojavascript.js
@@ -61,9 +61,66 @@ $(".gotoMain").on("click", function() {
 
 // Game INTF.
 
-var a = [
+let start = document.getElementById("start");
+let lvls = document.getElementById("lvlSel");
+//let lvls2 = document.getElementById("lvls-p2");
+let gmb = document.getElementById("gamebox");
+var stp = 0; var ltp = 0;
+
+const levels = [...document.querySelectorAll('.levelSelectNum')];
+const levelsGame = document.querySelectorAll('.level');
+for (const level of levels) {
+    level.addEventListener('click', showLevel);
+};
+
+// Shows Level From #lvlSel Num Click
+
+function showLevel() {
+    // hide everything first
+    //lvls2.style.display = "none";
+    for (const game of levelsGame) {
+        game.style.display = "none";
+        ltp =+ 1;
+        console.log(ltp);
+    }
+    let level = levels.indexOf(this);
+    let game = levelsGame[level];
+    lvls.style.display = "none";
+    gmb.style.display = "block";
+    game.style.display = 'block';
+};
+
+// Initially Starts Game && Changes ' > Start Game ' Text
+
+function startgame() {
+    $(".hiddenMain, #main").hide();
+    $("#gamebox").show();
+
+    if (stp >= 1 || ltp>=1 ) {
+        $("#gamebox").show();
+        // fix bug => After clicking 'Level Select' and selecting a level; anytime you go back to the main menu '>Start Game' txt doesn't change to '>Back to Game'
+    } else {
+        $("#gamebox").show();
+        $("#lvl1").show();
+        stp =+ 1;
+        document.getElementById('stgtxt').innerHTML = "> Back to Game";
+    };
+};
+
+    // Hints UI
+
+    $(".hintTop").on("click", function() {
+        $(this).hide();
+        $(this).next(".hintBtm").show(); ;
+    });
+
+
+
+
+
+var A = [
     "friendship",
-    "pancake",
+    "pancake", "pancakes",
     "fireman", "firemen",
     "sunflower",
     "mailbox",
@@ -86,7 +143,7 @@ var a = [
     "puss in boots",
     "aladdin",
     "tortoise and hare", "the tortoise and the hare", "tortoise and the hare",
-    "the little mermaid",
+    "the little mermaid", "little mermaid",
     "red riding hood", "little red riding hood",
     "goldilocks", "goldilocks and the three bears", "the three bears",
     "snow white",
@@ -97,69 +154,94 @@ var a = [
     "sleeping beauty",
     "the frog prince", "frog prince", "princess and the frog", "the princess and the frog",
     "turn up the music",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "baby, it's cold outside", "baby it's cold outside", "baby its cold outside",
+    "no photos allowed", "no photos",
+    "handicap accessible",
+    "free wifi",
+    "rock on",
+    "i believe you",
+    "music to my ears",
+    "i'm blessed",
+    "happy halloween", "happy halloween!",
+    "i love pizza",
+    "girls night out", "girl's night out",
+    "heard the news", "heard the news?",
+    "cross my heart", "cross your heart",
+    "time's running out", "times running out",
+    "oh my god",
+    "stop monkeying around",
+    "we come in peace",
+    "happy birthday", "happy birthday!",
+    "wanna build a snowman", "do you wanna build a snowman",
+    "water under the bridge",
+    "piece of cake",
+    "old news",
+    "break the ice",
+    "french kiss",
+    "lovebird", "lovebirds",
+    "starbucks",
+    "burger king",
+    "cowboy",
+    "cookie monster",
+    "queen bee", "queen b",
+    "pumpkin pie",
+    "corndog",
+    "apple tv",
+    "killer whale",
+    "rainbow",
+    "headlight", "headlights",
+    "wheelchair",
+    "spiderman",
+    "the walking dead",
+    "it",
+    "i love lucy",
+    "buffy the vampire slayer",
+    "the wizard of oz", "wizard of oz",
+    "godzilla",
+    "africa",
+    "north america",
+    "asia",
+    "europe",
+    "south america",
+    "antartica",
+    "austrailia",
+    "chinese new year", "chinese new years",
+    "hanukkah",
+    "marriage",
+    "easter",
+    "ramadan",
+    "earth day",
+    "diwali",
+    "day of the dead",
+    "apple",
+    "chik fil a",
+    "mc donald's", "mc donalds",
+    "amazon",
+    "sprint",
+    "windows"
 ]
 
-let start = document.getElementById("start");
-let lvls = document.getElementById("level-select");
-let lvls2 = document.getElementById("lvls-p2");
-let gmb = document.getElementById("gamebox");
-var stp = 0;
-var ltp = 0;
-
-const levels = [...document.querySelectorAll('.levelSelect')];
-const levelsGame = document.querySelectorAll('.level');
-for (const level of levels) {
-    level.addEventListener('click', showLevel);
-}
-
-function showLevel() {
-    // hide everything first
-    lvls2.style.display = "none";
-    for (const game of levelsGame) {
-        game.style.display = "none";
-        ltp =+ 1;
-        console.log(ltp);
-    }
-    let level = levels.indexOf(this);
-    let game = levelsGame[level];
-    lvls.style.display = "none";
-    gmb.style.display = "block";
-    game.style.display = 'block';
-}
 
 
-function startgame() {
-    $(".hiddenMain, #main").hide();
-    $("#gamebox").show();
+$(".subBtn").click(function() {
+    var userA = $(".input").val(); // gets input from input
+    console.log(userA);
 
-    if (stp >= 1 || ltp>=1 ) {
-        gmb.style.display = "block";
-    } else {
-        gmb.style.display = "block";
-        lvl1.style.display = "block";
-        stp =+ 1;
-        document.getElementById('stgtxt').innerHTML = "> Back to Game";
-    }
-};
+        // Updates Score
+        var scoreNum = $("#scoreNum").text();
+        var nextScoreNum = "";
+            if ($.inArray(userA, A) !=-1) {
+                // GO TO NEXT, BUT DO NOTHING RN
 
-    // Hints UI
+                    var thisStage = $(this).closest('.level');
+                    var nextStage = $(thisStage).next('.level');
 
-    $(".hintTop").on("click", function() {
-        $(this).hide();
-        $(this).next(".hintBtm").show(); ;
-    });
+                    thisStage.hide();
+                    nextStage.show();
+                    $('.input').val(""); // clears input boxggggg
+            } else {
+                // DO NOTHING
+                $('.input').val(""); // clears input box
+                console.log("hey... kids!");
+            }
+});
